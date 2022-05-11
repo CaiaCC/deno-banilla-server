435461f374a58d7685bb5a7e1c747b5a24686440
CaiaCC <caiachuang@gmail.com>
2022-05-11 12:45:49 -0700

    Finish up all other requirements.

--- README.md
+++ README.md
@@ -11,9 +11,7 @@
 1. [x] GET /hello -> will return the user-agent(a identifier what system user is using)
 2. [x] GET /hello/:name -> will return hello {name} (using: url pattern API,request API)
 3. [x] GET /echo?q=hi_monkey -> wil return the q param is: 'hi_monkey' ()
-4. [] GET /redirect -> redirect the user to the home page '/' with a 302 status code (using: Response API)
-5. [] POST /login -> will take a {name: 'bob', password: 'hello'}.
-        If the name is 'alice' and the password is 'redqueen' return 200 ok, otherwise 403 forbidden
-6. [ ] DELETE /todo/:id -> will take a body {name: 'bob', password: 'hello'}.
-        If the name is 'alice' and the password is 'redqueen' and the id is 42 -> return 200 ok, otherwise 403
-7. [ ] All other non-matching routes will return a 404 'Not Found'
+4. [x] GET /redirect -> redirect the user to the home page '/' with a 302 status code (using: Response API)
+5. [x] POST /login -> will take a {name: 'bob', password: 'hello'}. If the name is 'alice' and the password is 'redqueen' return 200 ok, otherwise 403 forbidden
+6. [x] DELETE /todo/:id -> will take a body {name: 'bob', password: 'hello'}. If the name is 'alice' and the password is 'redqueen' and the id is 42 -> return 200 ok, otherwise 403
+7. [x] All other non-matching routes will return a 404 'Not Found'
--- server.ts
+++ server.ts
@@ -15,59 +15,143 @@ for await (const conn of server) {
 		// The native HTTP server uses the web standard `Request` and `Response`
 		// objects.
 
-		const { headers, method, redirect, url } = request;
+		const { method, url } = request;
 
 		const baseUrl = new URL(url);
 		const { pathname } = baseUrl;
+		// console.log({ pathname });
+
+		// console.log("baseUrl: ", baseUrl);
 
 		// get param for /hello
 		const pattern = new URLPattern({ pathname: "/hello/:name" });
+		// console.log({ pattern });
 		const parameter = pattern.exec(url); // return 'bob' if /hello/bob
 		const name = pattern.exec(url)?.pathname.groups.name;
 
-
 		const test = pattern.test(url); //return true if pattern matches /hello/:name
 		// console.log(test, parameter && parameter.pathname.groups.name);
+		// console.log({pathname});
+
+		//match todo id
+		const pattern2 = new URLPattern({ pathname: "/todo/:id" });
+		const match2 = pattern2.exec(url);
+		const id = pattern2.exec(url)?.pathname.groups.id;
 
 		switch (pathname) {
 			case "/hello": {
-				const body = `Your user-agent is:\n\n${
+				if(method !== "GET") break;
+				const ResponseBody = `Your user-agent is:\n\n${
 					request.headers.get("user-agent") ?? "Unknown"
 				}`;
 				respondWith(
-					new Response(body, {
+					new Response(ResponseBody, {
 						status: 200,
 					})
 				);
 
 				break;
-			};
+			}
 			case `/hello/${name}`: {
-				const body = `Your name is ${name}`;
+				if (method !== "GET") break;
+				const ResponseBody = `Your name is ${name}`;
 				respondWith(
-					new Response(body, {
+					new Response(ResponseBody, {
 						status: 200,
 					})
 				);
 				break;
-			};
+			}
 			case "/echo": {
-				let query = baseUrl.searchParams.get("q")
-				const body = `Your query string is: ${query}`;
+				if (method !== "GET") break;
+				let query = baseUrl.searchParams.get("q");
+				const ResponseBody = `Your query string is: ${query}`;
 				respondWith(
-					new Response(body, {
+					new Response(ResponseBody, {
 						status: 200,
 					})
 				);
 
 				break;
-			};
+			}
+			case "/redirect": {
+				if (method !== "GET") break;
+				// respondWith(new Response.redirect(baseUrl.origin, 302));
+				respondWith(
+					new Response("", {
+						status: 302,
+						headers: new Headers({
+							location: "/",
+						}),
+					})
+				);
+				console.log("SUCCESSES REDIRECT TO: ", pathname);
+				break;
+			}
+
+			case "/login": {
+				if (method !== "POST") break;
+				const requestJson = await request.json();
+				const { name, password } = requestJson;
+				let statusCode = 403;
+
+				if (name === "alice" && password === "redqueen") {
+					statusCode = 200;
+					respondWith(
+						new Response("Hello alice", {
+							status: statusCode,
+						})
+					);
+				} else {
+					respondWith(
+						new Response("wrong password", {
+							status: statusCode,
+						})
+					);
+				}
+				break;
+			}
+			case `/todo/${id}`: {
+				if (method !== "DELETE") break;
+				const requestJson = await request.json();
+
+				const { name, password } = requestJson;
+				let statusCode = 403;
+
+				if (
+					name === "alice" &&
+					password === "redqueen" &&
+					id === "42"
+				) {
+					statusCode = 200;
+					respondWith(
+						new Response("deleted todo", {
+							status: statusCode,
+						})
+					);
+				} else {
+					respondWith(
+						new Response("no permissions", {
+							status: statusCode,
+						})
+					);
+				}
+				break;
+			}
+			case `/`: {
+				const body = `HOME`;
+				respondWith(
+					new Response(body, {
+						status: 200,
+					})
+				);
+				break;
+			}
 
 			default: {
-				const body = `Broken D:`;
 				respondWith(
-					new Response(body, {
-						status: 200,
+					new Response("Not found", {
+						status: 404,
 					})
 				);
 				break;
